13f6494ae8bc356c705e75b7692b23eca1e4bf5a
Pranjal Patel <patelpranjal139@gmail.com>
2023-11-05 00:30:37 +0530

    Updated

---
+++ 32_Use the Lifecycle Method componentWillMount.js
@@ -0,0 +1,13 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+      // Change code below this line
+      console.log("hello! My name is pranjal!")
+      // Change code above this line
+    }
+    render() {
+      return <div />
+    }
+  };
